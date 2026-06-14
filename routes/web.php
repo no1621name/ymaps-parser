69a12b706eb4508986e978017581d072972e4f53
George Sysuev <no1621name@gmail.com>
2026-06-14 23:39:03 +0300

    feat(routes): move auth routes to web.php, wrap API in sanctum middleware

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('app'))->name('app');
 
 Route::prefix('sanctum')->group(function () {
     Route::get('/csrf-cookie', fn () => response()->noContent())->name('sanctum.csrf-cookie');
+});
+
+Route::prefix('api')->group(function () {
+    Route::post('login', [LoginController::class, 'store'])->name('login');
+    Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
 });
 
 Route::get('{any}', fn () => view('app'))->where('any', '^((?!api).)*');
