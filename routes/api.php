af49059d92887943611b0b652bb7261b0137829b
George Sysuev <no1621name@gmail.com>
2026-06-14 15:21:35 +0300

    feat: add API routes for organizations and reviews

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\OrganizationController;
+use App\Http\Controllers\Api\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResources([
+    'organizations' => OrganizationController::class,
+], ['only' => ['index', 'show', 'store', 'destroy']]);
+
+Route::post('organizations/{organization}/refresh', [OrganizationController::class, 'refresh']);
+Route::get('organizations/{organization}/reviews', [ReviewController::class, 'index']);
